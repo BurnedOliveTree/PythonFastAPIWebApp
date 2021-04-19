049355cceec994feb2c485d9e6b0f9829abf48ee
Ksawery Chodyniecki <ksawery.chodyniecki@gmail.com>
2021-04-19 17:58:24 +0200

    added /method

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 
 app = FastAPI()
 
 @app.get('/')
 def root():
     return {'message': 'Hello world'}
+
+@app.get('/method')
+@app.put('/method')
+@app.options('/method')
+@app.delete('/method')
+@app.post('/method', status_code=201)
+def method(request: Request):
+    return {'method': request.method}
 
 @app.get('/hello/{name}')
 def hello_name_view(name: str):
--- test_main.py
+++ test_main.py
@@ -5,10 +5,27 @@ from main import app
 
 client = TestClient(app)
 
-def test_read_main():
+def test_read_root():
     response = client.get('/')
     assert response.status_code == 200
     assert response.json() == {'message': 'Hello world'}
+
+def test_read_method():
+    response = client.get('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'GET'}
+    response = client.put('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'PUT'}
+    response = client.options('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'OPTIONS'}
+    response = client.delete('/method')
+    assert response.status_code == 200
+    assert response.json() == {'method': 'DELETE'}
+    response = client.post('/method')
+    assert response.status_code == 201
+    assert response.json() == {'method': 'POST'}
 
 @pytest.mark.parametrize('name', ['Zenek', 'Marek', 'Alojzy'])
 def test_hello_name(name: str):
